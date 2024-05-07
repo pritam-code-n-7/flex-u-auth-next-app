192da7921132b8b47a6076108de356842d838441
pritam-code-n-7 <pritamimca1003@gmail.com>
2024-05-07 09:53:22 +0530

    I made this project successfully but initial and I made whole codebase responsive

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto py-10 min-h-screen">
       <div className="max-w-lg mx-auto border border-gray-300 p-6 rounded-md shadow-md">
         <h1 className="text-3xl font-semibold text-center mb-6">Welcome To Our Home</h1>
         <Link href="/" className="block text-center py-2 px-4 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 transition duration-300">
