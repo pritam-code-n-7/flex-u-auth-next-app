b2374aa210742842dd73ec8bfce5df8104ae6290
pritam-code-n-7 <pritamimca1003@gmail.com>
2024-05-07 08:16:06 +0530

    I finshed logged the object in console successfully on submit the form and fixed error types to ErrorOption to resolve property message does not exist on type object

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,19 +1,24 @@
 import { InputFieldT } from "@/types/FormTypes";
 import React from "react";
 
-const InputField = ({ type, name, placeholder, register, error }: InputFieldT) => {
+const InputField = ({
+  type,
+  name,
+  placeholder,
+  register,
+  error,
+}: InputFieldT) => {
   return (
     <div>
       <input
+        {...register(name)}
         type={type}
         name={name}
         id={`field_${name}`}
         placeholder={placeholder}
         className="rounded border border-neutral-500  w-full relative z-10  mt-2 outline-none bg-neutral-950 text-white focus:ring-2 focus:ring-teal-500 py-4 px-2 leading-tight text-center placeholder:opacity-45"
       />
-      {
-        error && <span className="text-red-500 py-1">{error?.message}</span>
-      }
+      {error && <span className="text-red-500 py-1">{error.message}</span>}
     </div>
   );
 };
--- src/types/FormTypes.ts
+++ src/types/FormTypes.ts
@@ -1,7 +1,9 @@
+import { ErrorOption } from "react-hook-form";
+
 export type InputFieldT = {
   type: string;
   placeholder: string;
   name: string;
   register: any;
-  error: undefined | object;
+  error: undefined | ErrorOption;
 };
